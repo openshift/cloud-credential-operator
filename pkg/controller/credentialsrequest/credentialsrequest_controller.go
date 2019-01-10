d673a0749a2289284cbfdb804e257a8182beae01
Devan Goodwin <dgoodwin@redhat.com>
2019-01-10 17:53:59 -0400

    Switch from DeploymentConfig to Deployment.

--- pkg/controller/credentialsrequest/credentialsrequest_controller.go
+++ pkg/controller/credentialsrequest/credentialsrequest_controller.go
@@ -207,6 +207,7 @@ type ReconcileCredentialsRequest struct {
 // TODO: temporary
 // +kubebuilder:rbac:groups=core,resources=configmaps,verbs=get
 // +kubebuilder:rbac:groups=core,resources=namespaces,verbs=get;list;watch
+// +kubebuilder:rbac:groups=config.openshift.io,resources=clusterversions,verbs=get;list
 func (r *ReconcileCredentialsRequest) Reconcile(request reconcile.Request) (reconcile.Result, error) {
 	logger := log.WithFields(log.Fields{
 		"controller": "credreq",
