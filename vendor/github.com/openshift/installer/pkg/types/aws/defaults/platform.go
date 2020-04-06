bbd93a9a52ebedd31646a6d0c1436043cf91f5cb
Seth Jennings <sjenning@redhat.com>
2020-04-06 12:59:38 -0500

    vendor: prune vendor tree

--- vendor/github.com/openshift/installer/pkg/types/aws/defaults/platform.go
+++
@@ -1,9 +0,0 @@
-package defaults
-
-import (
-	"github.com/openshift/installer/pkg/types/aws"
-)
-
-// SetPlatformDefaults sets the defaults for the platform.
-func SetPlatformDefaults(p *aws.Platform) {
-}
