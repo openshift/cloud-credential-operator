9a2517baae4620a58d9b33e7e42cf75fe7360c2a
staebler <staebler@redhat.com>
2020-05-22 11:31:52 -0400

    support other Azure cloud environments
    Use the Azure cloud environment specified by the
    `status.platformStatus.azure.cloudName` field of the
    infrastructure.config.openshift.io resource.
    
    See openshift/enhancements#321

--- pkg/azure/passthrough_test.go
+++ pkg/azure/passthrough_test.go
@@ -166,7 +166,7 @@ func TestPassthroughExists(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			f := fake.NewFakeClient(&validRootSecret, &validSecret)
-			actuator, err := azure.NewActuator(f)
+			actuator, err := azure.NewActuator(f, openshiftapiv1.AzurePublicCloud)
 			assert.Nil(t, err)
 
 			cr, err := newCredentialsRequest(tt.in)
@@ -197,7 +197,7 @@ func TestPassthroughCreate(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			f := fake.NewFakeClient(&validRootSecret, &validSecret, &clusterInfra, &clusterDNS)
-			actuator, err := azure.NewActuator(f)
+			actuator, err := azure.NewActuator(f, openshiftapiv1.AzurePublicCloud)
 			assert.Nil(t, err)
 
 			cr, err := newCredentialsRequest(tt.in)
@@ -238,7 +238,7 @@ func TestPassthroughUpdate(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			f := fake.NewFakeClient(&validRootSecret, &validSecret, &clusterInfra, &clusterDNS)
-			actuator, err := azure.NewActuator(f)
+			actuator, err := azure.NewActuator(f, openshiftapiv1.AzurePublicCloud)
 			assert.Nil(t, err)
 
 			cr, err := newCredentialsRequest(tt.in)
@@ -270,7 +270,7 @@ func TestPassthroughDelete(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			f := fake.NewFakeClient(&validRootSecret, &validSecret)
-			actuator, err := azure.NewActuator(f)
+			actuator, err := azure.NewActuator(f, openshiftapiv1.AzurePublicCloud)
 			assert.Nil(t, err)
 
 			cr, err := newCredentialsRequest(tt.in)
