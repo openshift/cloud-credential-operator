e8294adb03100cbe52a27e1b45cac623a66ab84f
Matthew Booth <mbooth@redhat.com>
2021-03-24 10:16:36 +0000

    openstack: Add stub secretannotator
    This secretannotator does nothing other than validate the configuration
    of the OpenStack root secret. It will raise an error if the
    configuration is anything other than the default or explicitly
    'Passthrough'.

---
+++ pkg/operator/secretannotator/openstack/reconciler.go
@@ -0,0 +1,182 @@
+/*
+Copyright 2021 The OpenShift Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package openstack
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+
+	operatorv1 "github.com/openshift/api/operator/v1"
+
+	"github.com/openshift/cloud-credential-operator/pkg/operator/constants"
+	"github.com/openshift/cloud-credential-operator/pkg/operator/metrics"
+	"github.com/openshift/cloud-credential-operator/pkg/operator/secretannotator/status"
+	secretutils "github.com/openshift/cloud-credential-operator/pkg/operator/secretannotator/utils"
+	statuscontroller "github.com/openshift/cloud-credential-operator/pkg/operator/status"
+	"github.com/openshift/cloud-credential-operator/pkg/operator/utils"
+)
+
+func NewReconciler(mgr manager.Manager) reconcile.Reconciler {
+	c := mgr.GetClient()
+	r := &ReconcileCloudCredSecret{
+		Client: c,
+		Logger: log.WithField("controller", constants.SecretAnnotatorControllerName),
+	}
+
+	s := status.NewSecretStatusHandler(c)
+	statuscontroller.AddHandler(constants.SecretAnnotatorControllerName, s)
+
+	return r
+}
+
+func cloudCredSecretObjectCheck(secret metav1.Object) bool {
+	return secret.GetNamespace() == constants.CloudCredSecretNamespace && secret.GetName() == constants.OpenStackCloudCredsSecretName
+}
+
+func Add(mgr manager.Manager, r reconcile.Reconciler) error {
+	// Create a new controller
+	c, err := controller.New(constants.SecretAnnotatorControllerName, mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to cluster cloud secret
+	p := predicate.Funcs{
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			return cloudCredSecretObjectCheck(e.MetaNew)
+		},
+		CreateFunc: func(e event.CreateEvent) bool {
+			return cloudCredSecretObjectCheck(e.Meta)
+		},
+		DeleteFunc: func(e event.DeleteEvent) bool {
+			return cloudCredSecretObjectCheck(e.Meta)
+		},
+	}
+	err = c.Watch(&source.Kind{Type: &corev1.Secret{}}, &handler.EnqueueRequestForObject{}, p)
+	if err != nil {
+		return err
+	}
+
+	err = secretutils.WatchCCOConfig(c, types.NamespacedName{
+		Namespace: constants.CloudCredSecretNamespace,
+		Name:      constants.OpenStackCloudCredsSecretName,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+var _ reconcile.Reconciler = &ReconcileCloudCredSecret{}
+
+type ReconcileCloudCredSecret struct {
+	client.Client
+	Logger log.FieldLogger
+}
+
+// Reconcile will typically annotate the cloud cred secret to indicate the capabilities of the cloud credentials:
+// 1) 'mint' for indicating that the creds can be used to create new sub-creds
+// 2) 'passthrough' for indicating that the creds are capable enough to potentially be used as-is
+// 3) 'insufficient' for indicating that the creds are not usable for the cluster
+// In the event that the operator config resource has specified a mode to operate under (mint/passthrough)
+// then skip trying to determine the capabilities, and just annotate the secret.
+// +kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;update
+func (r *ReconcileCloudCredSecret) Reconcile(request reconcile.Request) (returnResult reconcile.Result, returnErr error) {
+	start := time.Now()
+
+	defer func() {
+		dur := time.Since(start)
+		metrics.MetricControllerReconcileTime.WithLabelValues(constants.SecretAnnotatorControllerName).Observe(dur.Seconds())
+	}()
+
+	mode, conflict, err := utils.GetOperatorConfiguration(r.Client, r.Logger)
+	if err != nil {
+		r.Logger.WithError(err).Error("error checking operator configuration")
+		return reconcile.Result{}, err
+	}
+	if !utils.IsValidMode(mode) {
+		r.Logger.Errorf("invalid mode of %s set", mode)
+		return reconcile.Result{}, fmt.Errorf("invalide mode of %s set", mode)
+	}
+	if conflict {
+		r.Logger.Error("configuration conflict between legacy configmap and operator config")
+		return reconcile.Result{}, fmt.Errorf("configuration conflict")
+	}
+	if mode == operatorv1.CloudCredentialsModeManual {
+		r.Logger.Info("operator in disabled / manual mode")
+		return reconcile.Result{}, err
+	}
+	switch mode {
+	case operatorv1.CloudCredentialsModeDefault,
+		operatorv1.CloudCredentialsModePassthrough:
+	default:
+		const msg = "OpenStack only supports Passthrough mode"
+		r.Logger.Error(msg)
+		return reconcile.Result{}, fmt.Errorf(msg)
+	}
+
+	secret := &corev1.Secret{}
+	err = r.Get(context.Background(), request.NamespacedName, secret)
+	if err != nil {
+		r.Logger.Debugf("secret not found: %v", err)
+		return reconcile.Result{}, err
+	}
+
+	if mode != operatorv1.CloudCredentialsModeDefault {
+		annotation, err := utils.ModeToAnnotation(mode)
+		if err != nil {
+			r.Logger.WithError(err).Error("failed to convert operator mode to annotation")
+			return reconcile.Result{}, err
+		}
+		err = r.updateSecretAnnotations(secret, annotation)
+		if err != nil {
+			r.Logger.WithError(err).Error("errored while annotating secret")
+		}
+		return reconcile.Result{}, err
+	}
+
+	return reconcile.Result{}, nil
+}
+
+func (r *ReconcileCloudCredSecret) updateSecretAnnotations(secret *corev1.Secret, value string) error {
+	secretAnnotations := secret.GetAnnotations()
+	if secretAnnotations == nil {
+		secretAnnotations = map[string]string{}
+	}
+
+	secretAnnotations[constants.AnnotationKey] = value
+	secret.SetAnnotations(secretAnnotations)
+
+	return r.Update(context.TODO(), secret)
+}
---
+++ pkg/operator/secretannotator/openstack/reconciler_test.go
@@ -0,0 +1,221 @@
+/*
+Copyright 2021 The OpenShift Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package openstack
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	configv1 "github.com/openshift/api/config/v1"
+	operatorv1 "github.com/openshift/api/operator/v1"
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	schemeutils "github.com/openshift/cloud-credential-operator/pkg/util"
+)
+
+func init() {
+	log.SetLevel(log.DebugLevel)
+}
+
+const (
+	cloudsNoCACert = `
+clouds:
+  openstack:
+    auth:
+      auth_url: http://1.2.3.4:5000
+      password: password
+      project_domain_name: Default
+      project_name: openshift
+      user_domain_name: Default
+      username: openshift
+    identity_api_version: "3"
+    region_name: regionOne
+    verify: true
+`
+	cloudsWithCACert = `
+clouds:
+  openstack:
+    auth:
+      auth_url: http://1.2.3.4:5000
+      password: password
+      project_domain_name: Default
+      project_name: openshift
+      user_domain_name: Default
+      username: openshift
+    cacert: %s
+    identity_api_version: "3"
+    region_name: regionOne
+    verify: true
+`
+)
+
+func TestReconcileCloudCredSecret_Reconcile(t *testing.T) {
+	schemeutils.SetupScheme(scheme.Scheme)
+
+	infra := &configv1.Infrastructure{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "cluster",
+		},
+		Status: configv1.InfrastructureStatus{
+			Platform:           configv1.OpenStackPlatformType,
+			InfrastructureName: "test-cluster",
+		},
+	}
+
+	/*
+		Test parsing of CCO configuration and the resulting annotation of the
+		root secret. Most of this is boilerplate behaviour.
+
+		* An empty string mode means default behaviour: don't generate an error,
+		  don't annotate the secret
+		* If a legacy config map exists and conflicts with the configuration of CCO
+		  we should return an error
+		* If an invalid mode is specified we should return an error
+		* If the Passthrough mode is specified explicitly we should annotate the
+		  secret with this mode.
+		* If the Mint mode is specified explicitly we should return an error,
+		  because this is not supported by OpenStack.
+
+	*/
+	t.Run("Test operating mode", func(t *testing.T) {
+		legacyDisabledCM := &corev1.ConfigMap{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "cloud-credential-operator-config",
+				Namespace: "openshift-cloud-credential-operator",
+			},
+			Data: map[string]string{
+				"disabled": "true",
+			},
+		}
+
+		for _, tc := range [...]struct {
+			name           string
+			mode           string
+			existing       []runtime.Object
+			wantAnnotation string
+			wantErr        bool
+		}{
+			{
+				name:           "empty string",
+				mode:           "",
+				existing:       nil,
+				wantAnnotation: "",
+				wantErr:        false,
+			},
+			{
+				name:           "configuration conflict",
+				mode:           "Passthrough",
+				existing:       []runtime.Object{legacyDisabledCM},
+				wantAnnotation: "",
+				wantErr:        true,
+			},
+			{
+				name:           "invalid mode",
+				mode:           "invalid",
+				existing:       nil,
+				wantAnnotation: "",
+				wantErr:        true,
+			},
+			{
+				name:           "Passthrough",
+				mode:           "Passthrough",
+				existing:       nil,
+				wantAnnotation: "passthrough",
+				wantErr:        false,
+			},
+			{
+				name:           "Mint",
+				mode:           "Mint",
+				existing:       nil,
+				wantAnnotation: "",
+				wantErr:        true,
+			},
+		} {
+			t.Run(tc.name, func(t *testing.T) {
+				secret := testSecret("")
+				existing := append(tc.existing, infra, secret, testOperatorConfig(tc.mode))
+				fakeClient := fake.NewFakeClient(existing...)
+
+				r := &ReconcileCloudCredSecret{
+					Client: fakeClient,
+					Logger: log.WithField("controller", "testController"),
+				}
+				_, err := r.Reconcile(reconcile.Request{NamespacedName: types.NamespacedName{
+					Name:      "openstack-credentials",
+					Namespace: "kube-system",
+				}})
+
+				if tc.wantErr {
+					require.Error(t, err, "ReconcileCloudCredSecret.Reconcile() did not return expected error")
+				} else {
+					require.NoError(t, err, "ReconcileCloudCredSecret.Reconcile() returned unexpected error")
+				}
+
+				reconciledSecret := &corev1.Secret{}
+				err = fakeClient.Get(context.TODO(), client.ObjectKey{
+					Namespace: secret.Namespace,
+					Name:      secret.Name,
+				}, reconciledSecret)
+				require.NoError(t, err, "Failed to fetch secret after ReconcileCloudCredSecret.Reconcile()")
+
+				annotation := reconciledSecret.Annotations["cloudcredential.openshift.io/mode"]
+				assert.Equal(t, annotation, tc.wantAnnotation, "Secret annotation not set correctly")
+			})
+		}
+	})
+}
+
+func testOperatorConfig(mode string) *operatorv1.CloudCredential {
+	return &operatorv1.CloudCredential{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "cluster",
+		},
+		Spec: operatorv1.CloudCredentialSpec{
+			CredentialsMode: operatorv1.CloudCredentialsMode(mode),
+		},
+	}
+}
+
+func testSecret(caCert string) *corev1.Secret {
+	var clouds string
+	if caCert == "" {
+		clouds = cloudsNoCACert
+	} else {
+		clouds = fmt.Sprintf(cloudsWithCACert, caCert)
+	}
+
+	return &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "openstack-credentials",
+			Namespace: "kube-system",
+		},
+		Data: map[string][]byte{
+			"clouds.yaml": []byte(clouds),
+		},
+	}
+}
