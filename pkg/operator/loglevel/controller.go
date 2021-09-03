08956f38a41f9cc62f6b9e3921489044b29e90d9
Michael Washer <mwasher@redhat.com>
2021-09-03 12:43:54 +1000

    Add controller to update loglevel on cloudcredential cr changes
    This commit adds
    - a controller that updates logrus and klog level on CloudCredential updates
    - a unit-test for the controller
    - operator/loglevel vendor module from openshift/library-go

---
+++ pkg/operator/loglevel/controller.go
@@ -0,0 +1,153 @@
+/*
+Copyright 2020 The OpenShift Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package loglevel
+
+import (
+	"fmt"
+
+	log "github.com/sirupsen/logrus"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+
+	operatorv1 "github.com/openshift/api/operator/v1"
+	logLevelUtils "github.com/openshift/library-go/pkg/operator/loglevel"
+
+	"github.com/openshift/cloud-credential-operator/pkg/operator/status"
+	"github.com/openshift/cloud-credential-operator/pkg/operator/utils"
+)
+
+const (
+	controllerName = "loglevel"
+)
+
+// Add creates a new ConfigController and adds it to the Manager.
+func Add(mgr manager.Manager, kubeConfig string) error {
+	r := newReconciler(mgr)
+
+	// Create a new controller
+	c, err := controller.New(controllerName, mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to CloudCredential object and reconcile the loglevel changes
+	err = c.Watch(
+		&source.Kind{Type: &operatorv1.CloudCredential{}},
+		&handler.EnqueueRequestForObject{})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// newReconciler returns a new reconcile.Reconciler
+func newReconciler(mgr manager.Manager) reconcile.Reconciler {
+	r := &ReconcileCloudCredentialConfig{
+		Client: mgr.GetClient(),
+	}
+	status.AddHandler(controllerName, r)
+
+	return r
+}
+
+var _ reconcile.Reconciler = &ReconcileCloudCredentialConfig{}
+
+// ReconcileCloudCredentialConfig reconciles a CredentialConfig object
+type ReconcileCloudCredentialConfig struct {
+	client.Client
+}
+
+// Reconcile reads the state of the cluster for a CredentialConfig object and
+// makes changes based on the state read and what is in the CredentialConfig.LogLevel
+// Automatically generate RBAC rules to allow the Controller to read and write required types.
+// +kubebuilder:rbac:groups=operator.openshift.io,resources=cloudcredential/spec,verbs=get
+func (r *ReconcileCloudCredentialConfig) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	logger := log.WithFields(log.Fields{
+		"controller": controllerName,
+		"cr":         fmt.Sprintf("%s/%s", request.NamespacedName.Namespace, request.NamespacedName.Name),
+	})
+
+	// Check OperatorLogLevel and LogLevel
+	log.Debug("Checking log level for Reconcile request")
+	desiredOperatorLogLevel, err := utils.GetOperatorLogLevel(r, logger)
+	if err != nil {
+		return reconcile.Result{}, err
+	}
+	if !logLevelUtils.ValidLogLevel(desiredOperatorLogLevel) {
+		return reconcile.Result{}, fmt.Errorf("the operatorLogLevel defined for the cloudcredential object is invalid")
+	}
+
+	desiredLogLevel, err := utils.GetLogLevel(r, logger)
+	if err != nil {
+		return reconcile.Result{}, err
+	}
+
+	if !logLevelUtils.ValidLogLevel(desiredLogLevel) {
+		return reconcile.Result{}, fmt.Errorf("the logLevel defined for the cloudcredential object is invalid")
+	}
+
+	// Pick the logLevel with the highest verbosity level.
+	maxLogLevel := desiredOperatorLogLevel
+	if logLevelUtils.LogLevelToVerbosity(desiredLogLevel) > logLevelUtils.LogLevelToVerbosity(desiredOperatorLogLevel) {
+		maxLogLevel = desiredLogLevel
+	}
+
+	currentLogLevel, errBool := logLevelUtils.GetLogLevel()
+	if errBool {
+		log.Debug("Unable to get the current loglevel. Defaulting the current level to 'Normal'")
+	}
+
+	if currentLogLevel == maxLogLevel {
+		return reconcile.Result{}, nil
+	}
+
+	// Set klog logLevel to the desired level
+	err = logLevelUtils.SetLogLEvel(maxLogLevel)
+	if err != nil {
+		return reconcile.Result{}, err
+	}
+
+	log.Infof("klog level changed to %v", maxLogLevel)
+
+	// Set lorus logLevel to the desired level
+	logrusLevel := crLogLevelToLogrusLevel(maxLogLevel)
+	log.SetLevel(logrusLevel)
+	log.Infof("logrus level changed to %s", logrusLevel)
+
+	return reconcile.Result{}, nil
+}
+
+func crLogLevelToLogrusLevel(logLevel operatorv1.LogLevel) (logrusLevel log.Level) {
+	switch logLevel {
+	case operatorv1.Debug:
+		logrusLevel = log.DebugLevel
+	case operatorv1.Trace, operatorv1.TraceAll:
+		logrusLevel = log.TraceLevel
+	case operatorv1.Normal:
+		logrusLevel = log.InfoLevel
+	default:
+		logrusLevel = log.InfoLevel
+	}
+	return logrusLevel
+}
---
+++ pkg/operator/loglevel/controller_test.go
@@ -0,0 +1,114 @@
+package loglevel
+
+import (
+	"testing"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/scheme"
+
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	operatorv1 "github.com/openshift/api/operator/v1"
+	logLevelUtils "github.com/openshift/library-go/pkg/operator/loglevel"
+
+	"github.com/openshift/cloud-credential-operator/pkg/operator/constants"
+	schemeutils "github.com/openshift/cloud-credential-operator/pkg/util"
+)
+
+const (
+	testCRName    = "cluster"
+	testNamespace = "openshift-cloud-credential-operator"
+)
+
+type ExpectedCondition struct {
+	klogLevel   operatorv1.LogLevel
+	logrusLevel log.Level
+}
+
+func TestLogLevelCloudCredentialReconcile(t *testing.T) {
+	schemeutils.SetupScheme(scheme.Scheme)
+
+	tests := []struct {
+		name               string
+		existing           []runtime.Object
+		expectedConditions ExpectedCondition
+	}{
+		{
+			name: "set debug log level",
+			existing: []runtime.Object{
+				testOperatorConfig(operatorv1.Debug, operatorv1.Debug),
+			},
+			expectedConditions: ExpectedCondition{
+				logrusLevel: log.DebugLevel,
+				klogLevel:   operatorv1.Debug,
+			},
+		}, {
+			name: "set normal log level",
+			existing: []runtime.Object{
+				testOperatorConfig(operatorv1.Normal, operatorv1.Normal),
+			},
+			expectedConditions: ExpectedCondition{
+				logrusLevel: log.InfoLevel,
+				klogLevel:   operatorv1.Normal,
+			},
+		}, {
+			name: "ensure max loglevel is chosen",
+			existing: []runtime.Object{
+				testOperatorConfig(operatorv1.TraceAll, operatorv1.Normal),
+			},
+			expectedConditions: ExpectedCondition{
+				logrusLevel: log.TraceLevel,
+				klogLevel:   operatorv1.TraceAll,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			fakeClient := fake.NewFakeClient(test.existing...)
+			rccc := &ReconcileCloudCredentialConfig{
+				Client: fakeClient,
+			}
+
+			_, err := rccc.Reconcile(reconcile.Request{
+				NamespacedName: types.NamespacedName{
+					Name:      constants.CloudCredOperatorConfig,
+					Namespace: testNamespace,
+				},
+			})
+
+			require.NoError(t, err, "Unexpected error: %v", err)
+
+			klogLevel, _ := logLevelUtils.GetLogLevel()
+			logrusLevel := log.GetLevel()
+
+			// Check levels
+			assert.Equal(t, test.expectedConditions.klogLevel, klogLevel)
+			assert.Equal(t, test.expectedConditions.logrusLevel, logrusLevel)
+		})
+	}
+}
+
+func testOperatorConfig(logLevel, operatorLogLevel operatorv1.LogLevel) *operatorv1.CloudCredential {
+	conf := &operatorv1.CloudCredential{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: constants.CloudCredOperatorConfig,
+		},
+		Spec: operatorv1.CloudCredentialSpec{
+			CredentialsMode: operatorv1.CloudCredentialsModeDefault,
+			OperatorSpec: operatorv1.OperatorSpec{
+				LogLevel:         logLevel,
+				OperatorLogLevel: operatorLogLevel,
+			},
+		},
+	}
+
+	return conf
+}
