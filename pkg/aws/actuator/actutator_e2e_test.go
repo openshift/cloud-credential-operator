5e788d01a0a5c6ac400ad7a6316ce81f98812d8c
Brett Tofel <btofel@redhat.com>
2023-05-01 09:43:31 -0400

    Update pkg/aws/actuator/actutator_e2e_test.go
    Co-authored-by: Andrew Butcher <abutcher@redhat.com>

--- pkg/aws/actuator/actutator_e2e_test.go
+++ pkg/aws/actuator/actutator_e2e_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-// Test_CheckSTS runs end-to-end tests to verify the basics of an STS workflow work.
+// Test_CheckSTS runs end-to-end tests to verify that the results of an STS workflow are successful.
 
 func Test_CheckSTS(t *testing.T) {
 	t.Run("test of STS", func(t *testing.T) {
