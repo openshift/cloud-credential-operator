ebeb70dfe87f33fd1e2bd001b8f1fcedb3d75f49
Brett Tofel <btofel@redhat.com>
2023-06-11 10:02:52 +0200

    Put back debug logging aws-actuator

--- pkg/aws/actuator/actuator.go
+++ pkg/aws/actuator/actuator.go
@@ -310,7 +310,7 @@ func (a *AWSActuator) sync(ctx context.Context, cr *minterv1.CredentialsRequest)
 		return err
 	}
 	logger := a.getLogger(cr)
-	logger.Info("running sync")
+	logger.Debug("running sync")
 
 	// Should we update anything
 	needsUpdate, err := a.needsUpdate(ctx, cr)
@@ -323,7 +323,7 @@ func (a *AWSActuator) sync(ctx context.Context, cr *minterv1.CredentialsRequest)
 	}
 
 	if !needsUpdate {
-		logger.Info("credentials already up to date")
+		logger.Debug("credentials already up to date")
 		return nil
 	}
 	stsDetected, err := utils.IsTimedTokenCluster(a.Client, logger)
